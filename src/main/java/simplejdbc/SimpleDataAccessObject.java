710cd155b1d2cc2b2ffd08ec53e79f44ac3424f0
Rémi Bastide <Remi.Bastide@irit.fr>
2017-09-26 13:08:55 +0200

    Implémentation partielle du DAO

--- src/main/java/simplejdbc/SimpleDataAccessObject.java
+++ src/main/java/simplejdbc/SimpleDataAccessObject.java
@@ -5,6 +5,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.sql.DataSource;
 
 public class SimpleDataAccessObject {
@@ -22,17 +24,35 @@ public class SimpleDataAccessObject {
 	/**
 	 *
 	 * @return le nombre d'enregistrements dans la table CUSTOMER
-	 * @throws SQLException
+	 * @throws DAOException
 	 */
 	public int numberOfCustomers() throws DAOException {
-		throw new UnsupportedOperationException("Pas encore implémenté");
+		int result = 0;
+
+		String sql = "SELECT COUNT(*) AS NUMBER FROM CUSTOMER";
+		// Syntaxe "try with resources" 
+		// cf. https://stackoverflow.com/questions/22671697/try-try-with-resources-and-connection-statement-and-resultset-closing
+		try (Connection connection = myDataSource.getConnection(); // Ouvrir une connexion
+			Statement stmt = connection.createStatement(); // On crée un statement pour exécuter une requête
+			ResultSet rs = stmt.executeQuery(sql) // Un ResultSet pour parcourir les enregistrements du résultat
+			) {
+			if (rs.next()) { // Pas la peine de faire while, il y a 1 seul enregistrement
+				// On récupère le champ NUMBER de l'enregistrement courant
+				result = rs.getInt("NUMBER");
+			}
+		} catch (SQLException ex) {
+			Logger.getLogger("DAO").log(Level.SEVERE, null, ex);
+			throw new DAOException(ex.getMessage());
+		}
+
+		return result;
 	}
 
 	/**
 	 *
 	 * @param customerId la clé du client à recherche
 	 * @return le nombre de bons de commande pour ce client (table PURCHASE_ORDER)
-	 * @throws SQLException
+	 * @throws DAOException
 	 */
 	public int numberOfOrdersForCustomer(int customerId) throws DAOException {
 		throw new UnsupportedOperationException("Pas encore implémenté");
@@ -43,7 +63,7 @@ public class SimpleDataAccessObject {
 	 *
 	 * @param customedID la clé du CUSTOMER à rechercher
 	 * @return l'enregistrement correspondant dans la table CUSTOMER, ou null si pas trouvé
-	 * @throws SQLException
+	 * @throws DAOException
 	 */
 	CustomerEntity findCustomer(int customedID) throws DAOException {
 		throw new UnsupportedOperationException("Pas encore implémenté");
@@ -54,7 +74,7 @@ public class SimpleDataAccessObject {
 	 *
 	 * @param state l'état à rechercher (2 caractères)
 	 * @return la liste des clients habitant dans cet état
-	 * @throws SQLException
+	 * @throws DAOException
 	 */
 	List<CustomerEntity> customersInState(String state) throws DAOException {
 		throw new UnsupportedOperationException("Pas encore implémenté");
